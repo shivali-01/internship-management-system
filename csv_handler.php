b537030e8efdeccc2752e3c0853053d8d2025203
shivali-01 <shivali.gjimt@gmail.com>
2026-08-20 13:17:20 +0530

    Add files via upload

---
+++ csv_handler.php
@@ -0,0 +1,14 @@
+<?php
+function saveToCSV($data) {
+    $file = __DIR__ . '/applications.csv';
+    $file_exists = file_exists($file);
+    $fp = fopen($file, 'a');
+    if ($fp) {
+        if (!$file_exists) {
+            fputcsv($fp, ['ID', 'Company Name', 'Email', 'Industry', 'Status', 'Date']);
+        }
+        fputcsv($fp, $data);
+        fclose($fp);
+    }
+}
+?>
